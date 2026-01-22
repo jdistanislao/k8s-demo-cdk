df6c8713585be7851fe7aad9910e954ada3e5a1a
distanislaojmmfg <distanislao.j@mmfg.it>
2026-01-22 08:59:52 +0100

    add backend deployment, fix ingress class name

--- main.go
+++ main.go
@@ -30,6 +30,75 @@ func createNamespace(ctx *pulumi.Context) (*corev1.Namespace, error) {
 	return namespace, nil
 }
 
+func createBackendDeployment(ctx *pulumi.Context, namespace *corev1.Namespace) (*appsv1.Deployment, error) {
+	appName := "k8s-demo-backend"
+	appVersion := "0.1"
+	image := appName+":"+appVersion
+
+	appSelectorLabels := pulumi.StringMap{
+		"app.kubernetes.io/name": pulumi.String(appName),
+		"app.kubernetes.io/instance": pulumi.String(ctx.Stack()),
+	}
+
+	appLabels := pulumi.StringMap{
+		"app.kubernetes.io/name": pulumi.String(appName),
+		"app.kubernetes.io/instance": pulumi.String(ctx.Stack()),
+		"app.kubernetes.io/version": pulumi.String(appVersion),
+		"app.kubernetes.io/component": pulumi.String("api"),
+		"app.kubernetes.io/managed-by": pulumi.String("Pulumi-cdk"),
+	}
+
+	deployment, err := appsv1.NewDeployment(ctx, appName, &appsv1.DeploymentArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String(appName),
+				Labels: appLabels,
+				Namespace: namespace.Metadata.Name(),
+			},
+			Spec: &appsv1.DeploymentSpecArgs{
+				Selector: &metav1.LabelSelectorArgs{
+					MatchLabels: appSelectorLabels,
+				},
+				Replicas: pulumi.Int(3),
+				Template: &corev1.PodTemplateSpecArgs{
+					Metadata: &metav1.ObjectMetaArgs{
+						Labels: appLabels,
+					},
+					Spec: &corev1.PodSpecArgs{
+						Containers: corev1.ContainerArray{
+							&corev1.ContainerArgs{
+								Name:  pulumi.String(appName),
+								Image: pulumi.String(image),
+								ImagePullPolicy: pulumi.String("IfNotPresent"),
+								Ports: corev1.ContainerPortArray{
+									&corev1.ContainerPortArgs{
+										ContainerPort: pulumi.Int(8080),
+										Name: pulumi.String("http"),
+									},
+								},
+								LivenessProbe: &corev1.ProbeArgs{
+									HttpGet: &corev1.HTTPGetActionArgs{
+										Path: pulumi.String("/live"),
+										Port: pulumi.String("http"),
+									},
+								},
+								ReadinessProbe: &corev1.ProbeArgs{
+									HttpGet: &corev1.HTTPGetActionArgs{
+										Path: pulumi.String("/ready"),
+										Port: pulumi.String("http"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+		return deployment, nil
+}
+
 func createApiDeployment(ctx *pulumi.Context, namespace *corev1.Namespace) (*appsv1.Deployment, error) {
 	appName := "k8s-demo-api"
 	appVersion := "0.3"
@@ -129,7 +198,7 @@ func createApiService(ctx *pulumi.Context, deployment *appsv1.Deployment) (*core
 
 func createApiIngress(ctx *pulumi.Context, service *corev1.Service) (*networkingv1.Ingress, error) {
 	name := "k8s-demo-api"
-	ingressClass := "ngnix"
+	ingressClass := "nginx"
 	hostName := "demok.cdk.here"
 
 	ingress, error := networkingv1.NewIngress(ctx, name, &networkingv1.IngressArgs{
@@ -153,8 +222,8 @@ func createApiIngress(ctx *pulumi.Context, service *corev1.Service) (*networking
 									Service: &networkingv1.IngressServiceBackendArgs{
 										Name: pulumi.String(name),
 										Port: &networkingv1.ServiceBackendPortArgs{
-											Name: pulumi.String(name),
-											// Number: pulumi.Int(8080),
+											// Name: pulumi.String(name),
+											Number: pulumi.Int(8080),
 										},
 									},
 								},
@@ -182,6 +251,11 @@ func main() {
 			return err
 		}
 
+		backendDeployment, err := createBackendDeployment(ctx, namespace)
+		if err != nil {
+			return err
+		}
+
 		apiDeployment, err := createApiDeployment(ctx, namespace)
 		if err != nil {
 			return err
@@ -198,6 +272,7 @@ func main() {
 		}
 
 		ctx.Export("namespace", namespace.Metadata.Name())
+		ctx.Export("backendDeployment", backendDeployment.Metadata.Name())
 		ctx.Export("apiDeployment", apiDeployment.Metadata.Name())
 		ctx.Export("apiService", apiService.Metadata.Name())
 		ctx.Export("apiIngress", apiIngress.Metadata.Name())
