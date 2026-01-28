79bd9212aadbbb3b7d9cbcd0b115f0059d4d6b83
distanislaojmmfg <distanislao.j@mmfg.it>
2026-01-28 15:40:48 +0100

    refactor to separate files

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strconv"
-
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
@@ -44,377 +42,7 @@ func createNamespace(ctx *pulumi.Context) (*corev1.Namespace, error) {
 	return namespace, nil
 }
 
-func createRaftDeployment(ctx *pulumi.Context, namespace *corev1.Namespace, raftHeadlessServiceName string) (*DeplymentInfo, error) {
-	appName := "k8s-demo-raft"
-	appVersion := "latest"
-	appPortHttp := 8080
-	appPortRaft := 6666
-	image := appName + ":" + appVersion
-
-	appSelectorLabels := pulumi.StringMap{
-		"app.kubernetes.io/name":     pulumi.String(appName),
-		"app.kubernetes.io/instance": pulumi.String(ctx.Stack()),
-	}
-
-	appLabels := pulumi.StringMap{
-		"app.kubernetes.io/name":       pulumi.String(appName),
-		"app.kubernetes.io/instance":   pulumi.String(ctx.Stack()),
-		"app.kubernetes.io/version":    pulumi.String(appVersion),
-		"app.kubernetes.io/component":  pulumi.String("raft"),
-		"app.kubernetes.io/managed-by": pulumi.String("Pulumi-cdk"),
-	}
-
-	deployment, err := appsv1.NewDeployment(ctx, appName, &appsv1.DeploymentArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(appName),
-			Labels:    appLabels,
-			Namespace: namespace.Metadata.Name(),
-		},
-		Spec: &appsv1.DeploymentSpecArgs{
-			Selector: &metav1.LabelSelectorArgs{
-				MatchLabels: appSelectorLabels,
-			},
-			Replicas: pulumi.Int(3),
-			Template: &corev1.PodTemplateSpecArgs{
-				Metadata: &metav1.ObjectMetaArgs{
-					Labels: appLabels,
-				},
-				Spec: &corev1.PodSpecArgs{
-					Containers: corev1.ContainerArray{
-						&corev1.ContainerArgs{
-							Name:            pulumi.String(appName),
-							Image:           pulumi.String(image),
-							ImagePullPolicy: pulumi.String("IfNotPresent"),
-							Ports: corev1.ContainerPortArray{
-								&corev1.ContainerPortArgs{
-									ContainerPort: pulumi.Int(appPortHttp),
-									Name:          pulumi.String("http"),
-								},
-								&corev1.ContainerPortArgs{
-									ContainerPort: pulumi.Int(appPortRaft),
-									Name:          pulumi.String("raft"),
-								},
-							},
-							LivenessProbe: &corev1.ProbeArgs{
-								HttpGet: &corev1.HTTPGetActionArgs{
-									Path: pulumi.String("/live"),
-									Port: pulumi.String("http"),
-								},
-							},
-							ReadinessProbe: &corev1.ProbeArgs{
-								HttpGet: &corev1.HTTPGetActionArgs{
-									Path: pulumi.String("/ready"),
-									Port: pulumi.String("http"),
-								},
-							},
-							Env: corev1.EnvVarArray{
-								corev1.EnvVarArgs{
-									Name:  pulumi.String("DEMOK_NAMESPACE"),
-									Value: namespace.Metadata.Name(),
-								},
-								corev1.EnvVarArgs{
-									Name:  pulumi.String("DEMOK_RAFT_HEADLESS_SERVICE_NAME"),
-									Value: pulumi.String(raftHeadlessServiceName),
-								},
-							},
-							// Lifecycle: &corev1.LifecycleArgs{
-							// 	PreStop: &corev1.LifecycleHandlerArgs{
-							// 		Exec: &corev1.ExecActionArgs{
-							// 			Command: pulumi.ToStringArray([]string{"sh", "-c", "sleep", "30"}),
-							// 		},
-							// 	},
-							// },
-						},
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		return nil, err
-	}
-	return &DeplymentInfo{Deployment: deployment, Name: appName, Ports: []int{appPortHttp, appPortRaft}}, nil
-}
-
-func createRaftService(ctx *pulumi.Context, deployment *DeplymentInfo) (*ServiceInfo, error) {
-	name := deployment.Name
-
-	service, error := corev1.NewService(ctx, name, &corev1.ServiceArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(name),
-			Labels:    deployment.Deployment.Metadata.Labels(),
-			Namespace: deployment.Deployment.Metadata.Namespace(),
-		},
-		Spec: &corev1.ServiceSpecArgs{
-			Type: corev1.ServiceSpecTypeClusterIP,
-			Ports: corev1.ServicePortArray{
-				&corev1.ServicePortArgs{
-					Name:       pulumi.String(deployment.Name+"-http"),
-					Port:       pulumi.Int(deployment.Ports[0]),
-					TargetPort: pulumi.String("http"),
-				},
-				&corev1.ServicePortArgs{
-					Name:       pulumi.String(deployment.Name+"-raft"),
-					Port:       pulumi.Int(deployment.Ports[1]),
-					TargetPort: pulumi.String("raft"),
-				},
-			},
-			Selector: deployment.Deployment.Spec.Selector().MatchLabels(),
-		},
-	})
-	if error != nil {
-		return nil, error
-	}
-	return &ServiceInfo{Service: service, Name: name}, nil
-}
-
-func createRaftHeadlessService(ctx *pulumi.Context, deployment *DeplymentInfo, serviceName string) (*ServiceInfo, error) {
-	
-	service, error := corev1.NewService(ctx, serviceName, &corev1.ServiceArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(serviceName),
-			Labels:    deployment.Deployment.Metadata.Labels(),
-			Namespace: deployment.Deployment.Metadata.Namespace(),
-		},
-		Spec: &corev1.ServiceSpecArgs{
-			ClusterIP: pulumi.String("None"),
-			Ports: corev1.ServicePortArray{
-				&corev1.ServicePortArgs{
-					Name:       pulumi.String(deployment.Name),
-					Port:       pulumi.Int(deployment.Ports[1]),
-					TargetPort: pulumi.String("raft"),
-				},
-			},
-			Selector: deployment.Deployment.Spec.Selector().MatchLabels(),
-		},
-	})
-	if error != nil {
-		return nil, error
-	}
-	return &ServiceInfo{Service: service, Name: serviceName}, nil
-}
-
-func createBackendDeployment(ctx *pulumi.Context, namespace *corev1.Namespace) (*DeplymentInfo, error) {
-	appName := "k8s-demo-backend"
-	appVersion := "latest"
-	appPort := 8080
-	image := appName + ":" + appVersion
-
-	appSelectorLabels := pulumi.StringMap{
-		"app.kubernetes.io/name":     pulumi.String(appName),
-		"app.kubernetes.io/instance": pulumi.String(ctx.Stack()),
-	}
-
-	appLabels := pulumi.StringMap{
-		"app.kubernetes.io/name":       pulumi.String(appName),
-		"app.kubernetes.io/instance":   pulumi.String(ctx.Stack()),
-		"app.kubernetes.io/version":    pulumi.String(appVersion),
-		"app.kubernetes.io/component":  pulumi.String("api"),
-		"app.kubernetes.io/managed-by": pulumi.String("Pulumi-cdk"),
-	}
-
-	deployment, err := appsv1.NewDeployment(ctx, appName, &appsv1.DeploymentArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(appName),
-			Labels:    appLabels,
-			Namespace: namespace.Metadata.Name(),
-		},
-		Spec: &appsv1.DeploymentSpecArgs{
-			Selector: &metav1.LabelSelectorArgs{
-				MatchLabels: appSelectorLabels,
-			},
-			Replicas: pulumi.Int(3),
-			Template: &corev1.PodTemplateSpecArgs{
-				Metadata: &metav1.ObjectMetaArgs{
-					Labels: appLabels,
-				},
-				Spec: &corev1.PodSpecArgs{
-					Containers: corev1.ContainerArray{
-						&corev1.ContainerArgs{
-							Name:            pulumi.String(appName),
-							Image:           pulumi.String(image),
-							ImagePullPolicy: pulumi.String("IfNotPresent"),
-							Ports: corev1.ContainerPortArray{
-								&corev1.ContainerPortArgs{
-									ContainerPort: pulumi.Int(appPort),
-									Name:          pulumi.String("http"),
-								},
-							},
-							LivenessProbe: &corev1.ProbeArgs{
-								HttpGet: &corev1.HTTPGetActionArgs{
-									Path: pulumi.String("/actuator/health/liveness"),
-									Port: pulumi.String("http"),
-								},
-							},
-							ReadinessProbe: &corev1.ProbeArgs{
-								HttpGet: &corev1.HTTPGetActionArgs{
-									Path: pulumi.String("/actuator/health/readiness"),
-									Port: pulumi.String("http"),
-								},
-							},
-							Lifecycle: &corev1.LifecycleArgs{
-								PreStop: &corev1.LifecycleHandlerArgs{
-									Exec: &corev1.ExecActionArgs{
-										Command: pulumi.ToStringArray([]string{"sh", "-c", "sleep", "30"}),
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		return nil, err
-	}
-	return &DeplymentInfo{Deployment: deployment, Name: appName, Port: appPort}, nil
-}
-
-func createBackendService(ctx *pulumi.Context, deployment *DeplymentInfo) (*ServiceInfo, error) {
-	name := deployment.Name
-	port := deployment.Port
-
-	service, error := corev1.NewService(ctx, name, &corev1.ServiceArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(name),
-			Labels:    deployment.Deployment.Metadata.Labels(),
-			Namespace: deployment.Deployment.Metadata.Namespace(),
-		},
-		Spec: &corev1.ServiceSpecArgs{
-			Type: corev1.ServiceSpecTypeClusterIP,
-			Ports: corev1.ServicePortArray{
-				&corev1.ServicePortArgs{
-					Name:       pulumi.String(deployment.Name),
-					Port:       pulumi.Int(deployment.Port),
-					TargetPort: pulumi.String("http"),
-				},
-			},
-			Selector: deployment.Deployment.Spec.Selector().MatchLabels(),
-		},
-	})
-	if error != nil {
-		return nil, error
-	}
-	return &ServiceInfo{Service: service, Name: name, Port: port}, nil
-}
-
-func createApiDeployment(ctx *pulumi.Context, namespace *corev1.Namespace, backendService *ServiceInfo) (*DeplymentInfo, error) {
-	appName := "k8s-demo-api"
-	appVersion := "latest"
-	appPort := 8080
-	image := appName + ":" + appVersion
-
-	appSelectorLabels := pulumi.StringMap{
-		"app.kubernetes.io/name":     pulumi.String(appName),
-		"app.kubernetes.io/instance": pulumi.String(ctx.Stack()),
-	}
-
-	appLabels := pulumi.StringMap{
-		"app.kubernetes.io/name":       pulumi.String(appName),
-		"app.kubernetes.io/instance":   pulumi.String(ctx.Stack()),
-		"app.kubernetes.io/version":    pulumi.String(appVersion),
-		"app.kubernetes.io/component":  pulumi.String("api"),
-		"app.kubernetes.io/managed-by": pulumi.String("Pulumi-cdk"),
-	}
-
-	deployment, err := appsv1.NewDeployment(ctx, appName, &appsv1.DeploymentArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(appName),
-			Labels:    appLabels,
-			Namespace: namespace.Metadata.Name(),
-		},
-		Spec: &appsv1.DeploymentSpecArgs{
-			Selector: &metav1.LabelSelectorArgs{
-				MatchLabels: appSelectorLabels,
-			},
-			Replicas: pulumi.Int(3),
-			Template: &corev1.PodTemplateSpecArgs{
-				Metadata: &metav1.ObjectMetaArgs{
-					Labels: appLabels,
-				},
-				Spec: &corev1.PodSpecArgs{
-					Containers: corev1.ContainerArray{
-						&corev1.ContainerArgs{
-							Name:            pulumi.String(appName),
-							Image:           pulumi.String(image),
-							ImagePullPolicy: pulumi.String("IfNotPresent"),
-							Ports: corev1.ContainerPortArray{
-								&corev1.ContainerPortArgs{
-									ContainerPort: pulumi.Int(appPort),
-									Name:          pulumi.String("http"),
-								},
-							},
-							LivenessProbe: &corev1.ProbeArgs{
-								HttpGet: &corev1.HTTPGetActionArgs{
-									Path: pulumi.String("/actuator/health/liveness"),
-									Port: pulumi.String("http"),
-								},
-							},
-							ReadinessProbe: &corev1.ProbeArgs{
-								HttpGet: &corev1.HTTPGetActionArgs{
-									Path: pulumi.String("/actuator/health/readiness"),
-									Port: pulumi.String("http"),
-								},
-							},
-							Env: corev1.EnvVarArray{
-								corev1.EnvVarArgs{
-									Name:  pulumi.String("DEMOK_BACKEND_SERVICE_NAME"),
-									Value: pulumi.String(backendService.Name),
-								},
-								corev1.EnvVarArgs{
-									Name:  pulumi.String("DEMOK_BACKEND_SERVICE_PORT"),
-									Value: pulumi.String(strconv.Itoa(backendService.Port)),
-								},
-							},
-							Lifecycle: &corev1.LifecycleArgs{
-								PreStop: &corev1.LifecycleHandlerArgs{
-									Exec: &corev1.ExecActionArgs{
-										Command: pulumi.ToStringArray([]string{"sh", "-c", "sleep", "30"}),
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		return nil, err
-	}
-	return &DeplymentInfo{Deployment: deployment, Name: appName, Port: appPort}, nil
-}
-
-func createApiService(ctx *pulumi.Context, deployment *DeplymentInfo) (*ServiceInfo, error) {
-	name := deployment.Name
-	port := deployment.Port
-
-	service, error := corev1.NewService(ctx, name, &corev1.ServiceArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(name),
-			Labels:    deployment.Deployment.Metadata.Labels(),
-			Namespace: deployment.Deployment.Metadata.Namespace(),
-		},
-		Spec: &corev1.ServiceSpecArgs{
-			Type: corev1.ServiceSpecTypeClusterIP,
-			Ports: corev1.ServicePortArray{
-				&corev1.ServicePortArgs{
-					Name:       pulumi.String(name),
-					Port:       pulumi.Int(port),
-					TargetPort: pulumi.String("http"),
-				},
-			},
-			Selector: deployment.Deployment.Spec.Selector().MatchLabels(),
-		},
-	})
-	if error != nil {
-		return nil, error
-	}
-	return &ServiceInfo{Service: service, Name: name, Port: port}, nil
-}
-
-func createApiIngress(ctx *pulumi.Context, service *ServiceInfo) (*networkingv1.Ingress, error) {
+func createIngress(ctx *pulumi.Context, service *ServiceInfo, raftService *ServiceInfo) (*networkingv1.Ingress, error) {
 	name := service.Name
 	ingressClass := "nginx"
 	hostName := "demok.cdk.here"
@@ -436,6 +64,8 @@ func createApiIngress(ctx *pulumi.Context, service *ServiceInfo) (*networkingv1.
 					Http: &networkingv1.HTTPIngressRuleValueArgs{
 						Paths: networkingv1.HTTPIngressPathArray{
 							&networkingv1.HTTPIngressPathArgs{
+								Path:     pulumi.String("/test"),
+								PathType: pulumi.String("Prefix"),
 								Backend: networkingv1.IngressBackendArgs{
 									Service: &networkingv1.IngressServiceBackendArgs{
 										Name: pulumi.String(name),
@@ -444,32 +74,32 @@ func createApiIngress(ctx *pulumi.Context, service *ServiceInfo) (*networkingv1.
 										},
 									},
 								},
-								Path:     pulumi.String("/test"),
-								PathType: pulumi.String("Prefix"),
 							},
 							&networkingv1.HTTPIngressPathArgs{
-								Backend: networkingv1.IngressBackendArgs{
-									Service: &networkingv1.IngressServiceBackendArgs{
-										Name: pulumi.String("k8s-demo-raft"),
-										Port: &networkingv1.ServiceBackendPortArgs{
-											Number: pulumi.Int(6666),
-										},
-									},
-								},
 								Path:     pulumi.String("/raft"),
 								PathType: pulumi.String("Prefix"),
-							},
-							&networkingv1.HTTPIngressPathArgs{
 								Backend: networkingv1.IngressBackendArgs{
 									Service: &networkingv1.IngressServiceBackendArgs{
-										Name: pulumi.String("k8s-demo-raft"),
+										Name: pulumi.String(raftService.Name),
 										Port: &networkingv1.ServiceBackendPortArgs{
-											Number: pulumi.Int(8080),
+											// Number: pulumi.Int(6666),
+											Name: pulumi.String("raft"),
 										},
 									},
 								},
+							},
+							&networkingv1.HTTPIngressPathArgs{
 								Path:     pulumi.String("/"),
 								PathType: pulumi.String("Prefix"),
+								Backend: networkingv1.IngressBackendArgs{
+									Service: &networkingv1.IngressServiceBackendArgs{
+										Name: pulumi.String(raftService.Name),
+										Port: &networkingv1.ServiceBackendPortArgs{
+											// Number: pulumi.Int(8080),
+											Name: pulumi.String("http"),
+										},
+									},
+								},
 							},
 						},
 					},
@@ -491,6 +121,10 @@ func main() {
 			return err
 		}
 
+		/////////////////////////////
+		// BACKEND
+		/////////////////////////////
+
 		backendDeployment, err := createBackendDeployment(ctx, namespace)
 		if err != nil {
 			return err
@@ -500,6 +134,10 @@ func main() {
 		if err != nil {
 			return err
 		}
+
+		/////////////////////////////
+		// API
+		/////////////////////////////
 
 		apiDeployment, err := createApiDeployment(ctx, namespace, backendService)
 		if err != nil {
@@ -511,23 +149,31 @@ func main() {
 			return err
 		}
 
-		apiIngress, err := createApiIngress(ctx, apiService)
-		if err != nil {
-			return err
-		}
+		/////////////////////////////
+		// RAFT
+		/////////////////////////////
 
 		raftHeadlessServiceName := "k8s-demo-raft-headless"
 		raftDeployment, err := createRaftDeployment(ctx, namespace, raftHeadlessServiceName)
 		if err != nil {
 			return err
 		}
-		
+
 		raftService, err := createRaftService(ctx, raftDeployment)
 		if err != nil {
 			return err
 		}
-		
+
 		raftHeadlessService, err := createRaftHeadlessService(ctx, raftDeployment, raftHeadlessServiceName)
+		if err != nil {
+			return err
+		}
+
+		/////////////////////////////
+		// INGRESS
+		/////////////////////////////
+
+		ingress, err := createIngress(ctx, apiService, raftService)
 		if err != nil {
 			return err
 		}
@@ -540,7 +186,7 @@ func main() {
 		ctx.Export("backendService", backendService.Service.Metadata.Name())
 		ctx.Export("apiDeployment", apiDeployment.Deployment.Metadata.Name())
 		ctx.Export("apiService", apiService.Service.Metadata.Name())
-		ctx.Export("apiIngress", apiIngress.Metadata.Name())
+		ctx.Export("ingress", ingress.Metadata.Name())
 
 		return nil
 	})
